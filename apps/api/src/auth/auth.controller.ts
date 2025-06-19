e9e34f0499bc043e9f1b27c722102c20c58b16fc
Dev-scott <sadoscott25@gmail.com>
2025-06-19 23:34:50 +0100

    refactor

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
   @UseGuards(GoogleAuthGuard)
   @Get('google/callback')
   async googleCallback(@Request() req, @Res() res: Response) {
-    // console.log('user:', req.user);
+    console.log('user in callback:', req.user);
 
     const userData = await this.authService.login(req.user);
 
